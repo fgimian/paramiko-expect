dc8367c1749d9c35db0eb8bd43a88c9927df0636
Kiseok Kim <kiseok7@gmail.com>
2017-05-12 08:33:21 +0000

    deleted indent

--- examples/paramiko_expect-demo.py
+++ examples/paramiko_expect-demo.py
@@ -35,22 +35,22 @@ def main():
         # Create a client interaction class which will interact with the host
         with SSHClientInteraction(client, timeout=10, display=True) as interact:
             interact.expect(PROMPT)
-    
+
             # Run the first command and capture the cleaned output, if you want
             # the output without cleaning, simply grab current_output instead.
             interact.send('uname -a')
             interact.expect(PROMPT)
             cmd_output_uname = interact.current_output_clean
-    
+
             # Now let's do the same for the ls command but also set a timeout for
             # this specific expect (overriding the default timeout)
             interact.send('ls -l /')
             interact.expect(PROMPT, timeout=5)
             cmd_output_ls = interact.current_output_clean
-    
+
             # To expect multiple expressions, just use a list.  You can also
             # selectively take action based on what was matched.
-    
+
             # Method 1: You may use the last_match property to find out what was
             # matched
             interact.send('/vagrant/examples/paramiko_expect-demo-helper.py')
@@ -58,7 +58,7 @@ def main():
             if interact.last_match == 'Please enter your name: ':
                 interact.send('Fotis Gimian')
                 interact.expect(PROMPT)
-    
+
             # Method 2: You may use the matched index to determine the last match
             # (like pexpect)
             interact.send('/vagrant/examples/paramiko_expect-demo-helper.py')
@@ -66,7 +66,7 @@ def main():
             if found_index == 1:
                 interact.send('Fotis Gimian')
                 interact.expect(PROMPT)
-    
+
             # Send the exit command and expect EOF (a closed session)
             interact.send('exit')
             interact.expect()
