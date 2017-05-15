d0dcf6f4a4ddab2bd38748dfcbdb7f5a40be4017
Israel Fruchter <ifruchte@cisco.com>
2017-05-15 23:57:15 +0300

    version 0.2.8

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst') as f:
 
 setup(
     name='paramiko-expect',
-    version='0.2.7',
+    version='0.2.8',
     url='https://github.com/fgimian/paramiko-expect',
     license='MIT',
     author='Fotis Gimian',
