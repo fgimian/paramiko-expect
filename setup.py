8fa9640885d3be9aa15a1b9d95521c9a1a896474
Israel Fruchter <fruch@scylladb.com>
2021-12-27 23:17:17 +0200

    Remove python27 support

--- setup.py
+++ setup.py
@@ -24,10 +24,12 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Intended Audience :: Developers'
     ]
